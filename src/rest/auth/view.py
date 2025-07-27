591c5edca9c503fce153a0469502f0974bbb3642
tighwm <partnyor117@gmail.com>
2025-07-27 21:36:35 +0300

    some base header nav

--- src/rest/auth/view.py
+++ src/rest/auth/view.py
@@ -1,3 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+
+from utils.templates import templates
 
 router = APIRouter(prefix="/auth")
+
+
+@router.get("/", name="auth-index")
+async def index_login(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="auth/index.html",
+    )
