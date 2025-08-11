97a077211069ad20d274f6a439d0e84ba3e9cf59
tighwm <partnyor117@gmail.com>
2025-08-11 17:14:25 +0300

    creating minio buckets in lifespan

--- src/create_app.py
+++ src/create_app.py
@@ -5,10 +5,12 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 
 from core.models import db_helper
+from utils import minio_helper
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    await minio_helper.create_buckets_if_not_exists(["novel-cover"])
     yield
     await db_helper.dispose()
 
